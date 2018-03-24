3b0bfd47936e3e800fbfbbf98fa0fb53fe3398e5
Anton Davydov <antondavydov.o@gmail.com>
2018-03-24 13:43:16 +0300

    Use do notation for get request

--- lib/dry/http/client/request/get.rb
+++ lib/dry/http/client/request/get.rb
@@ -9,16 +9,24 @@ module Dry
         class Get
           include Dry::Monads::Result::Mixin
           include Dry::Monads::Try::Mixin
+          include Dry::Monads::Do.for(:call)
 
           def call(url, options)
-            Try(URI::InvalidURIError) { URI.parse(url) }.bind do |uri|
-              request = Net::HTTP::Get.new(uri.request_uri)
-              yield request if block_given?
-              to_result http(uri).request(request)
-            end
+            uri = yield parse_url(url)
+            request = Net::HTTP::Get.new(uri.request_uri)
+            send_request(uri, request)
           end
 
         private
+
+          def parse_url(url)
+            Try(URI::InvalidURIError) { URI.parse(url) }
+          end
+
+          def send_request(uri, request)
+            response = http(uri).request(request)
+            response.is_a?(Net::HTTPSuccess) ? Success(response) : Failure(response)
+          end
 
           def http(uri)
             http = Net::HTTP.new(uri.host, uri.port)
@@ -31,10 +39,6 @@ module Dry
               http.use_ssl = true
               http.verify_mode = OpenSSL::SSL::VERIFY_NONE
             end
-          end
-
-          def to_result(response)
-            response.is_a?(Net::HTTPSuccess) ? Success(response) : Failure(response)
           end
         end
       end
